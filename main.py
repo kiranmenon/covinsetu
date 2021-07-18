440085cf5a76a48ffeb050c2169d969e300bc967
kiranmenon <targetkiranp@gmail.com>
2021-07-18 11:44:05 +0530

    get by center added

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from apiclient import calendarByPin
+from apiclient import calendarByCenter
 
 class Tasks:
     @staticmethod
     def do_calendarByPin():
             resource = calendarByPin.CalendarByPin()
             resource.exec()
+    
+    @staticmethod
+    def do_calendarByCenter():
+        rsrc = calendarByCenter.CalendarByCenter()
+        rsrc.exec()
 
 if __name__ == "__main__":
     Tasks.do_calendarByPin()
+    Tasks.do_calendarByCenter()
+
     
     
